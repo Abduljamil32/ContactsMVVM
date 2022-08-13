d851b25d7c61edd0e5bb8748650bba93e6f9eb60
co.abu <anizomxoja@gmail.com>
2022-08-13 18:11:22 +0500

    Initial Commit

---
+++ ContactsMVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsMVVM
+//
+//  Created by Avaz Mukhitdinov on 13/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
